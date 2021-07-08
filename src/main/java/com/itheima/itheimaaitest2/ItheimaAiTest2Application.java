2710087a34b4910a5abed1493f16151d9ab76eba
wangh <wanghongjun1982@gmail.com>
2021-07-08 23:16:23 +0800

    人工智能—鉴黄

---
+++ src/main/java/com/itheima/itheimaaitest2/ItheimaAiTest2Application.java
@@ -0,0 +1,13 @@
+package com.itheima.itheimaaitest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItheimaAiTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItheimaAiTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/itheima/itheimaaitest2/ItheimaAiTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.itheimaaitest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItheimaAiTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
